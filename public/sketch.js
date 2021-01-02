6a69a7ecb3e7fc2a3e501b1e1d5768557d789dc5
Jimmy <jimmystry@gmail.com>
2021-01-02 14:18:08 -0500

    Old code from p5js sketch (baseline)

---
+++ public/sketch.js
@@ -0,0 +1,238 @@
+// Set up variables, vals should not be changed
+const board = [];
+
+let placedFlags = [];
+let numPlacedFlags = 0;
+
+let firstClick = true;
+let playerAlive = true;
+
+// Constants, vals can be played around with
+// Default values for 30x16 board (standard) is square length of 20, board width of 630, board height of 336
+const squareWidth = 20;
+const squareHeight = 20;
+
+const xIncrement = squareWidth + 1;
+const yIncrement = squareHeight + 1;
+
+let boardWidth = 800;
+let boardHeight = 500;
+
+let headerWidth = boardWidth;
+const headerHeight = 50;
+
+let numSquaresX = 40;
+let numSquaresY = 30;
+
+const startingMines = 150;
+
+//setNumSquaresFromDimensions();
+setDimensionsFromNumSquares();
+
+
+// Creates a timer and increments it every second
+let time = 0;
+const timer = setInterval(() => {
+    if (playerAlive && !checkWin())
+      time++
+  }, 1000);
+
+// setup is a custom p5 js function that runs once at the program's start and does not repeat
+// Sets up the board of mines
+function setup() {
+  createCanvas((boardWidth > headerWidth) ? boardWidth : headerWidth, boardHeight + headerHeight);
+  createBoard();
+}
+
+// draw is a custom p5 js function that loops continually throughout the program's operation
+// Loop through board and show each square
+function draw() {
+  background("#616363");
+  
+  // Show each square on the board
+  for (let i = 0; i < numSquaresY; i++) {
+    for (let j = 0; j < numSquaresX; j++) {
+      board[i][j].show();
+    }
+  }
+    
+  // Checks if the player has won/lost
+  if (checkWin())
+    alertText("You win!");
+  else if (!playerAlive)
+    alertText("You lose :(");
+  
+  // Displays number of remaining mines and the time
+  textSize(50);
+  textAlign(LEFT, BASELINE);
+  fill(255, 0, 0);
+  text(startingMines - numPlacedFlags, 50, headerHeight - 5);
+  text(time, width - 100, headerHeight - 5);
+}
+
+// Alerts the msg text in big, white letters across the screen
+function alertText(msg) {
+  textAlign(CENTER);
+  fill(255);
+  strokeWeight(5);
+  textSize(64);
+  text(msg, width / 2, height / 2);
+  //noLoop();
+  //clearInterval(timer);
+}
+
+// Custom p5 js event handler
+// Activates whenever mouse is pressed
+function mousePressed() {
+  if (!playerAlive)
+    return;
+  
+  // First, check if mouse is within bounds of board
+  // (mouseX and mouseY are values given by p5.js)
+  if (mouseX <= width && mouseY <= height && mouseY > headerHeight) {
+    // Use x and y of mouse to determine which square the mouse is over
+    const i = floor((mouseY - headerHeight) / yIncrement);
+    const j = floor(mouseX / xIncrement);
+    
+    // Select correct square from array
+    const clickedSquare = board[i][j];
+    
+    // Left click = click the square
+    if (mouseButton == LEFT) {
+      if (firstClick)
+        setMines(j, i);
+      
+      clickedSquare.click(true);
+      
+    // Right click = flag the square
+    } else if (mouseButton == RIGHT) {
+      clickedSquare.flag();
+    }
+  }
+}
+
+// Custom p5 js event handler that activates every key press
+function keyPressed() {
+  // If "f" is pressed, toggle between fullscreen and minimized modes
+  if (key == "f") {
+    let isFullscreen = fullscreen();
+    if (isFullscreen)
+      fullscreen(false);
+    else
+      fullscreen(true);
+  } else if (key == "r") {
+    restart();
+  }
+}
+
+// Checks if the player has won
+function checkWin() {
+  // Determines if the # of placed flags equals the # of mines
+  const allFlags = numPlacedFlags == startingMines;
+  
+  // Checks if all flags are correctly placed
+  const correctFlags = checkFlagPosition();
+  
+  // Only returns true if both conditions are met
+  return allFlags && correctFlags;
+}
+
+// Checks if all the placed flags are in the correct position
+function checkFlagPosition() {
+  let allFlagsCorrect = true;
+  
+  for (let f of placedFlags) {
+    if (!f.isMine)
+      allFlagsCorrect = false;
+  }
+  
+  return allFlagsCorrect;
+}
+
+// Set the board full of empty squares, 
+// then fills it with mines and calculates number of adjacent mines for each square
+function createBoard() {
+  for (let i = 0; i < numSquaresY; i++) {
+    board[i] = [];
+    for (let j = 0; j < numSquaresX; j++) {
+      board[i][j] = new Square(i, j, j * xIncrement, i * yIncrement + headerHeight);
+    }
+  }
+}
+
+// Randomly places startingMines # of mines throughout the board
+function setMines(safeCol, safeRow) {
+  let numMines = startingMines;
+  while (numMines > 0) {
+    const i = floor(random(0, numSquaresY));
+    const j = floor(random(0, numSquaresX));
+
+    if (!board[i][j].isMine && (Math.abs(i - safeRow) > 1 || Math.abs(j - safeCol) > 1)) {
+      board[i][j].isMine = true;
+      numMines--;
+    }
+  }
+  
+  findMines();
+}
+
+// Increments through each square and calculates adjacent mines for that square
+function findMines() {
+  for (let i = 0; i < numSquaresY; i++) {
+    for (let j = 0; j < numSquaresX; j++) {
+      board[i][j].calculateAdjacentMines();
+    }
+  }
+}
+
+// Resets the board and starts a new game
+function restart() {
+  firstClick = true;
+  playerAlive = true;
+  
+  placedFlags = [];
+  numPlacedFlags = 0;
+  
+  time = 0;
+  
+  createBoard();
+}
+
+// Adds a flag to the array of placed flags
+function addToFlagArray(square) {
+  placedFlags.push(square);
+  numPlacedFlags++;
+}
+
+// Removes a flag from the array of placed flags
+function removeFromFlagArray(square) {
+  const index = placedFlags.indexOf(square);
+  const temp = placedFlags[placedFlags.length - 1];
+  placedFlags[placedFlags.length - 1] = placedFlags[index];
+  placedFlags[index] = temp;
+  placedFlags.pop();
+  numPlacedFlags--;
+}
+
+// Sets the number of squares on the board based on predetermined dimensions
+function setNumSquaresFromDimensions() {
+  while (numSquaresX * xIncrement + xIncrement < boardWidth) {
+    numSquaresX++;
+  }
+
+  while (numSquaresY * yIncrement + yIncrement < boardHeight) {
+    numSquaresY++;
+  }
+}
+
+// Sets the board's dimensions based on the number of squares 
+function setDimensionsFromNumSquares() {
+  boardWidth = numSquaresX * xIncrement;
+  headerWidth = boardWidth;
+  boardHeight = numSquaresY * yIncrement;
+}
+
+// This event listener prevents the normal right-click menu from opening when you flag a square
+document.addEventListener("contextmenu", e => {
+  e.preventDefault();
+});
---
+++ public/square.js
@@ -0,0 +1,232 @@
+class Square {
+    constructor(row, col, x, y) {
+        this.row = row;
+        this.col = col;
+        this.x = x;
+        this.y = y;
+        this.numAdjacentMines = 0;
+        this.isMine = false;
+        this.isRevealed = false;
+        this.isFlagged = false;
+        this.textColor = "#9ea2a3";
+    }
+
+    flag() {
+        if (this.isFlagged) {
+            this.isRevealed = false;
+            this.isFlagged = false;
+            removeFromFlagArray(this);
+        } else if (!this.isRevealed) {
+            this.isRevealed = true;
+            this.isFlagged = true;
+            addToFlagArray(this);
+        }
+    }
+
+    // When this square is (left) clicked, we must determine whether it is a mine or not
+    // If not, we must determine the number of mines in adjacent squares
+    // This method is NOT a pre-built event handler
+    click(playerClick) {
+        // If this is the first click and the square is a mine, we must move the mine and make the clicked square safe
+        if (this.isMine && firstClick) {
+            this.isMine = false;
+            this.isRevealed = true;
+
+            // Update this square after mine moves
+            this.calculateAdjacentMines();
+
+            // Update the mine count of adjacent squares as well
+            const code = this.getAdjacentSquaresCode();
+            const adjacentSquares = this.getAdjacentSquares(code);
+
+            for (let i = 0; i < adjacentSquares.length; i++) {
+                adjacentSquares[i].calculateAdjacentMines();
+            }
+
+            // If it's the first click, move the mine to the upper-left hand corner and update those adjacent squares
+            board[0][0].isMine = true;
+
+            const cornerCode = board[0][0].getAdjacentSquaresCode();
+            const cornerSquares = board[0][0].getAdjacentSquares(cornerCode);
+
+            for (let i = 0; i < cornerSquares.length; i++) {
+                cornerSquares[i].calculateAdjacentMines();
+            }
+
+            // If this square has 0 adjacent mines, reveal the adjacent squares
+            if (this.numAdjacentMines == 0) {
+                this.revealAdjacentSquares();
+            }
+
+            firstClick = false;
+            return;
+        } else {
+            firstClick = false;
+        }
+
+        // If it's a mine and not flagged, player loses
+        if (this.isMine && !this.isFlagged) {
+            playerAlive = false;
+            this.isRevealed = true;
+            console.log("exploded");
+            // Don't click if the tile is flagged
+        } else if (!this.isFlagged) {
+            // If the player is clicking on a square that's already been clicked on, reveal the adjacent squares
+            if (this.isRevealed && playerClick)
+                this.revealAdjacentSquares();
+
+            this.isRevealed = true;
+
+            // If this square has 0 adjacent mines, reveal the adjacent squares
+            if (this.numAdjacentMines == 0) {
+                this.revealAdjacentSquares();
+            }
+        }
+    }
+
+    // Get adjacent squares and reveal them
+    revealAdjacentSquares() {
+        const code = this.getAdjacentSquaresCode();
+        const adjacentSquares = this.getAdjacentSquares(code);
+        for (let s of adjacentSquares) {
+            if (!s.isRevealed && !s.isFlagged)
+                s.click(false);
+        }
+    }
+
+    // Get adjacent squares and count the mines 
+    calculateAdjacentMines() {
+        const code = this.getAdjacentSquaresCode();
+        const adjacentSquares = this.getAdjacentSquares(code);
+        let numMines = 0;
+
+        for (let s of adjacentSquares) {
+            if (s.isMine)
+                numMines++;
+        }
+        this.numAdjacentMines = numMines;
+        this.updateColor();
+    }
+
+    // Get code for the adjacent squares
+    getAdjacentSquaresCode() {
+        let code = [1, 1, 1, 1, 0, 1, 1, 1, 1];
+        // This code represents which squares adjacent to the clicked square should be "selected"
+        // Code is arranged in order below (with 0 being the clicked square's position):
+        // 1  1  1    0 1 2
+        // 1  0  1    3 4 5
+        // 1  1  1    6 7 8
+
+        // If it's on a the top/bottom, deselect the squares on the top/bottm rows
+        if (this.row == 0) {
+            code[0] = 0;
+            code[1] = 0;
+            code[2] = 0;
+        } else if (this.row == numSquaresY - 1) {
+            code[6] = 0;
+            code[7] = 0;
+            code[8] = 0;
+        }
+
+        // If it's on a wall, deselect the left/right columns
+        if (this.col == 0) {
+            code[0] = 0;
+            code[3] = 0;
+            code[6] = 0;
+        } else if (this.col == numSquaresX - 1) {
+            code[2] = 0;
+            code[5] = 0;
+            code[8] = 0;
+        }
+
+        return code;
+    }
+
+    // Returns an array of all the adjacent squares using the code
+    getAdjacentSquares(code) {
+        const adjacentSquares = [];
+        let k = 0;
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                if (code[k] == 1) {
+                    adjacentSquares.push(board[this.row + i][this.col + j]);
+                }
+
+                k++;
+            }
+        }
+
+        return adjacentSquares;
+    }
+
+    // Updates the text's color based on number of adjacent mines
+    updateColor() {
+        switch (this.numAdjacentMines) {
+            case 0:
+                this.textColor = "#9ea2a3";
+                break;
+            case 1:
+                this.textColor = "#346beb";
+                break;
+            case 2:
+                this.textColor = "#21a336";
+                break;
+            case 3:
+                this.textColor = "#f20c14";
+                break;
+            case 4:
+                this.textColor = "#54178a";
+                break;
+            case 5:
+                this.textColor = "#8c1626";
+                break;
+            case 6:
+                this.textColor = "#5991ab";
+                break;
+            case 7:
+                this.textColor = "#1a4a35";
+                break;
+            default:
+                this.textColor = "#000000";
+                break;
+        }
+    }
+
+    // Show the tile
+    show() {
+        // Draws the tile itself
+        noStroke();
+        fill("#afb2b3");
+        rect(this.x, this.y, 20, 20);
+        stroke(255);
+        strokeWeight(1);
+        line(this.x + 2, this.y + 2, this.x + 2, this.y + 18);
+        line(this.x + 2, this.y + 2, this.x + 18, this.y + 2);
+        noStroke();
+
+        // Only display a symbol if the flag is revealed
+        if (this.isRevealed) {
+            if (this.isFlagged) {
+                // Flag symbol
+                fill(255, 0, 0);
+                triangle(this.x + 5, this.y + 5, this.x + 15, this.y + 8, this.x + 5, this.y + 11);
+                stroke(0);
+                strokeWeight(1);
+                line(this.x + 5, this.y + 5, this.x + 5, this.y + 18);
+            } else if (this.isMine) {
+                // Simple circle for mine
+                fill(0);
+                ellipse(this.x + 10, this.y + 10, 10);
+            } else {
+                // Text
+                fill("#9ea2a3");
+                rect(this.x, this.y, 20, 20);
+
+                fill(this.textColor);
+                textAlign(CENTER, TOP);
+                textSize(12);
+                text(this.numAdjacentMines, this.x + 10, this.y + 5);
+            }
+        }
+    }
+}
