65b5ebcba24403e514ec5a7991864629a20a38ea
Jimmy <jimmystry@gmail.com>
2021-01-02 14:51:03 -0500

    Handles basic event emitting

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import http from 'http';
+import { Socket } from 'socket.io';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const io = require('socket.io')(http.createServer(app));
-
 app.use(express.static('public'));
 
-app.listen(3000);
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket: Socket) => {
+    console.log('user connected');
+    
+    socket.on('debug', (msg: Object) => {
+        console.log(msg);
+    });
+});
+
+server.listen(3000);
 console.log(`Listening on port ${PORT}`);
